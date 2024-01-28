4a33da46cfefd8d26359fd2ee196488ee82dc744
Edsoncortes40 <saul_cortes00@yahoo.com>
2024-01-28 13:51:54 -0500

    built 360p conversion POST endpoint, /process-video

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,14 +1,35 @@
 import express  from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json()); //tell express to use json for Request body format.
 
 //This an HTTP GET endpoint
-app.get("/", (req, res) =>{
-    res.send("Hello World!");
+app.post("/process-video", (req, res) =>{
+    //Get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath){
+        res.status(400).send("Bad Request: Missing file Paths.");
+    }
+
+    //This function is Asynchronous
+    ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360') //converting the video file into 360p resolution
+    .on('end', function() {
+        res.status(200).send("Video Processing Finished Successful!");
+    })
+    .on("error", function(err: any) {
+        console.log('An error occured:' + err.message);
+        res.status(500).send('Internal Server Error:' + err.message);
+    })
+    .save(outputFilePath);
+
 });
 
+const port = 3000;
 app.listen(port, () => {
     console.log(
-        'Video processing service listening at http://localhost:${port}');
+        'Video processing service listening at http://localhost:' + port);
 });
