0366330ddcccaa99b23004e7ca697298c2675386
Edsoncortes40 <saul_cortes00@yahoo.com>
2024-02-07 13:29:05 -0500

    Fixing quick error

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -10,10 +10,10 @@ export default async function Home() {
     <main>
       {
         videos.map((video) => (
-          <Link href={"/watch?v=" + video.filename}>
+          <Link href={"/watch?v=" + video.filename} key={video.id}>
             <Image src={'/thumbnail.png'} alt='video' width={120} height={80}
             className={styles.thumbnail}/>
-      </Link>
+          </Link>
         ))
       }
     </main>
